672eda5f7a0801f0b29ce750b5d22e199c6a339e
Eric Mignardi <mignardi.e@gmail.com>
2026-03-09 06:43:04 -0400

    Setup typer CLI skeleton, scapy to broadcast an ARP request to subnet, parsed ARP responses to print IP and MAC addresses

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from netscope!")
+from scapy.all import *
+import typer
 
+app = typer.Typer()
 
+destination_subnet = "192.168.1.0/24"
+
+@app.command()
+def scan():
+    print("Running scan...")
+    eth = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=destination_subnet)
+    print(f"Sending packet to {destination_subnet}")
+    answered, unanswered = srp(eth, timeout=2, verbose=0)
+    for sent, received in answered:
+        print(f"IP: {received.psrc} MAC: {received.hwsrc}")
+    
 if __name__ == "__main__":
-    main()
+    app()
